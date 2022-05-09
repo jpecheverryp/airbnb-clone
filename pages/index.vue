67cb6db8d26157c290018b3b12eb46e18384b03d
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-09 19:48:23 -0400

    created home card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-<h1>HELLO WORLD</h1>
+    <div v-for="home in homes" :key="home.objectID" style="float: left; margin:10px">
+      <home-card :home='home' />
+    </div>
   </div>
 </template>
 
 <script>
+  import homes from '~/data/homes'
+  import HomeCard from '../components/HomeCard.vue'
   export default {
-    
+    data()  {
+      return {
+        homes: homes.slice(0, 3)
+      }
+    },
+    components: {
+      HomeCard,
+    }
   }
 </script>
 
