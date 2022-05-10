999d76e3848b9dd4da1ec32fc875fb9b183057a2
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-10 16:04:38 -0400

    add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="text-align: center">
+    <img src="https://cdn.custom-cursor.com/cursors/chrome_dino_t-rex_455.png" alt="">
+    <br />
+    <br />
+    <h1>ERROR</h1>
+    {{error.message}} - {{error.statusCode}}
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Error'
+      }
+    },
+    props: {
+      error: {
+        type: Object,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
