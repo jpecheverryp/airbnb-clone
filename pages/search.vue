405beffbc3ddc4ce88b1f081e0d50858c9f642a7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-11 03:52:49 -0400

    add home row component to display search results

--- pages/search.vue
+++ pages/search.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     {{ lat }} / {{ lng }} - {{ label }}<br/>
-    <div v-for="home in homes" :key="home.objectID">{{ home.title }}</div>
+    <div v-if="homes.length > 0">
+      <home-row v-for="home in homes" :key="home.objectID" :home="home" />
+    </div>
+    <div v-else>
+      No Results Found
+    </div>
   </div>  
 </template>
 
 <script>
   export default {
+    head (){
+      return {
+        title: `Homes around ${this.label}`
+      }
+    },
+
     async beforeRouteUpdate(to, from, next){
       const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
       this.homes = data.json.hits
